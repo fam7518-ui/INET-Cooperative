949b90a6194c3b7bbe4c56b62db4ff3f121f2613
Fatima Mad-adam <fam7518@gmail.com>
2026-08-27 22:20:31 +0700

    Update README.md

---
+++ Week01/practice-vue/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+// นำเข้า TodoList (เช็ค path ให้ตรงกับที่อยู่ไฟล์จริง)
+import TodoList from './components/icons/TodoList.vue';
+</script>
+
+<template>
+  <!-- เรียกใช้งานคอมโพเนนต์ตรงนี้ -->
+  <TodoList />
+</template>
---
+++ Week01/practice-vue/src/components/icons/TodoList.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from 'vue';
+
+let id = 0;
+const todos = ref([
+    {id: id++, work: "read book"},
+    {id: id++, work: "code"},
+    {id: id++, work: "sleep"},
+]);
+
+const newTodo = ref("");
+
+const addTodo = () => {
+    let todoOb = {
+        id: id++,
+        work: newTodo.value,
+
+    };
+    todos.value.push(todoOb);
+};
+
+const delTodo = (todo) => {
+    console.log(todo);
+    todos.value = todos.value.filter((td)=>td != todo)
+
+}
+</script>
+
+<template>
+    <h1> TODO List</h1>
+    <input v-model = "newTodo" />
+    <button @click = "addTodo()"> Add </button> 
+    <ul>
+        <li v-for = "todo in todos" :key = "todo.id">
+        <h2> {{  todo.work }}</h2>
+        <button @click="delTodo(todo)" class = "close">X</button>
+        </li>
+    </ul>
+</template> 
