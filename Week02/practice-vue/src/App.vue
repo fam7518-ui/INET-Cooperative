5b1f6ff0a06562981d8503594f2179b17a5caa6d
Fatima Mad-adam <fam7518@gmail.com>
2026-09-04 15:53:47 +0700

    Update README and add Week 02

---
+++ Week02/practice-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="padding: 20px;">
+    <h2>ทดลอง v-model </h2>
+    
+    <!-- ช่อง Input ที่ผูกกับตัวแปร message -->
+    <input v-model="message" placeholder="ลองพิมพ์ข้อความตรงนี้..." />
+
+    <!-- แสดงผลค่าของตัวแปร message ทันที -->
+    <p>ข้อความของคุณคือ: <strong>{{ message }}</strong></p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// สร้างตัวแปรมารับค่าและตั้งค่าเริ่มต้นเป็นค่าว่าง
+const message = ref('')
+</script>
